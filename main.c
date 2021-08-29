fd5270d7bd411e56e77e47cb2f0bde4f988f03cf
IgnacioCabreraS <ignacio.cabrera.s@mail.pucv.cl>
2021-08-29 00:29:26 -0400

    test

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "list.h"
+
+int main(){
+    printf("toy volao alo");
+    return 0;
+}
