13ce12ba75e16c06a0a808615f8c57d28d66f3d9
IgnacioCabreraS <ignacio.cabrera.s@mail.pucv.cl>
2021-08-29 00:45:25 -0400

    real main

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-#include <assert.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "list.h"
 
 int main(){
     printf("toy volao alo\n");
-    printf ("otro print señores");
+    printf ("otro print señores\n");
     printf("a");
     return 0;
 }
